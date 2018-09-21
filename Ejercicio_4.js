0d0dd1d869679e6e43ee258107ce32b08ab5b4b7
Roberto Rubio <37192655+RR1998@users.noreply.github.com>
2018-09-21 09:03:31 -0600

    Update Ejercicio_4.js

--- Ejercicio_4.js
+++ Ejercicio_4.js
@@ -1,14 +1,5 @@
-var numero = prompt("ingrese un numero");
-var aux = "";
+var num = prompt("ingrese un numero");
+var aux= num.toString();
 
-    aux = aux.concat(numero%2);
-    console.log(numero);
-    numero = parseInt(Math.floor(numero/2));
-    console.log(numero);
-while(numero/2 != 0 || numero/2 != 1){
-    aux = aux.concat(numero%2);
-    numero = Math.floor(numero/2);
-    console.log("aiuda!")
-}
-    aux = aux.concat(numero%2);
-console.log(aux.split("").reverse().join(""));
+
+parseInt(aux, 2);
